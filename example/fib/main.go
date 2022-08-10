5257abf90eb2d85b9cf7babf9c31402204fa1691
hongbo.mo <hongbo.mo@upai.com>
2022-08-10 16:48:30 +0800

    hack: fib example spans export to jaeger

--- example/fib/main.go
+++ example/fib/main.go
@@ -23,6 +23,7 @@ import (
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
@@ -36,7 +37,7 @@ func newExporter(w io.Writer) (trace.SpanExporter, error) {
 		// Use human readable output.
 		stdouttrace.WithPrettyPrint(),
 		// Do not print timestamps for the demo.
-		stdouttrace.WithoutTimestamps(),
+		//		stdouttrace.WithoutTimestamps(),
 	)
 }
 
@@ -54,30 +55,44 @@ func newResource() *resource.Resource {
 	return r
 }
 
+const (
+	service     = "trace-demo"
+	environment = "production"
+	id          = 1
+)
+
+// tracerProvider returns an OpenTelemetry TracerProvider configured to use
+// the Jaeger exporter that will send spans to the provided url. The returned
+// TracerProvider will also use a Resource configured with all the information
+// about the application.
+func tracerProvider(url string) (*trace.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := trace.NewTracerProvider(
+		// Always be sure to batch in production.
+		trace.WithBatcher(exp),
+		// Record information about this application in a Resource.
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(service),
+			attribute.String("environment", environment),
+			attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
+}
+
 func main() {
 	l := log.New(os.Stdout, "", 0)
 
-	// Write telemetry data to a file.
-	f, err := os.Create("traces.txt")
+	tp, err := tracerProvider("http://localhost:14268/api/traces")
 	if err != nil {
-		l.Fatal(err)
-	}
-	defer f.Close()
-
-	exp, err := newExporter(f)
-	if err != nil {
-		l.Fatal(err)
+		log.Fatal(err)
 	}
 
-	tp := trace.NewTracerProvider(
-		trace.WithBatcher(exp),
-		trace.WithResource(newResource()),
-	)
-	defer func() {
-		if err := tp.Shutdown(context.Background()); err != nil {
-			l.Fatal(err)
-		}
-	}()
 	otel.SetTracerProvider(tp)
 
 	sigCh := make(chan os.Signal, 1)
